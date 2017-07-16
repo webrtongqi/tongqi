5c832a9b0fd27917b6d4ad79cf8fc6572f7c21c8
rongtongqi <rongtongqi@51kupai.com>
2017-07-16 14:35:23 +0800

    chish

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,11 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import axios from 'axios'
+import axios from 'axios'
 import App from './App'
 import router from './router'
-
-// Vue.prototype.$axios = axios
+ Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
