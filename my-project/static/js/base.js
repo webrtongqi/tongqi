4558dc9048260722199415940e33281a9af90ca8
webrtonhqi <webrtongqi@163.com>
2017-10-31 00:20:39 +0800

    ss

--- my-project/static/js/base.js
+++ my-project/static/js/base.js
@@ -5,7 +5,7 @@ exports.install = function (Vue, options) {
             switch (data.bidStatus) {
                 case 2:
                     _leftPrice = '<p style="color:#B28147">起拍价&yen;' + data.initialPrice + '</p>';
-                    _rightPrice = '<p>保证金&yen;' +data.bailPrice + '</p>';
+                    _rightPrice = '<p>1保证金&yen;' +data.bailPrice + '</p>';
                     break;
                 case 3:
                     _leftPrice = '<p style="color:#C4311D">当前价&yen;' + data.maxPrice + '</p>';
